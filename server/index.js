cd78134bdc08c722cb70f73bf0b9d3a9ece2ec10
divyanshi-agr <divsagr3@gmail.com>
2020-07-27 22:38:00 +0530

    server1

--- server/index.js
+++ server/index.js
@@ -12,8 +12,10 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection');
 
-    socket.on('join', ({name,room}) => {
+    socket.on('join', ({name,room}, callback) => {
         console.log(name, room);
+
+        callback();
     })
 
     socket.on('disconnect', () => {
