72290a9c2e80eacb17e7fe2db19e7b38679ff956
divyanshi-agr <divsagr3@gmail.com>
2020-08-01 23:40:51 +0530

    new7

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const router = require('./router');
 
 // const app = express();
 // const server = http.createServer(app);
-// const io = socketio(server);
+const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log('We have a new connection');
