56248f7e124f2e476066db2e35f2078752a295c0
divyanshi-agr <divsagr3@gmail.com>
2020-08-01 23:39:20 +0530

    new5

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
 // const app = express();
-const server = http.createServer(app);
+// const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
